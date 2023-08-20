c9ff5dff1acf43c994129c83925ee96bf9bb297d
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-08-20 07:46:19 -0400

    Multi Deploy

--- deploy-function.sh
+++ deploy-function.sh
@@ -2,18 +2,11 @@
 
 FILE=./.appwrite # The path of the Appwrite file
 
-if $1; then # Check whether action is in single function mode
-    if [ -f "$FILE" ]; then # Check whether the Appwrite file exists
-        . $FILE # set values from .appwrite file as variables
-        appwrite functions createTag --functionId=$FUNCID --command="${COMMAND}" --code="${CODE}" # deploy the fucntion to Appwrite
-    fi
-else # Action is in multiple functions mode
-    for d in ./*/; do # Go through each subdirectory
+for d in ./*/; do # Go through each subdirectory
         cd "$d" # cd into the directory
         if [ -f "$FILE" ]; then # Check whether the Appwrite file exists
             . $FILE # set values from .appwrite file as variables
-            appwrite functions createDeployment --functionId=$FUNCID --activate=true --entrypoint="index.js" --code="${CODE}" 
+            appwrite functions createDeployment --functionId=$FUNCID --activate=true --entrypoint="index.js" --code="${CODE}" --endpoint=$1
         fi
         cd .. # cd back to root directory
-    done
-fi
+done
